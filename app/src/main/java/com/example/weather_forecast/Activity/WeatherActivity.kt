f8120b297ac66e90609cf47261d0985d1b190268
Artem <a.colomeytsev@list.ru>
2022-12-20 16:53:45 +0300

    - создал Prisenter

--- app/src/main/java/com/example/weather_forecast/Activity/WeatherActivity.kt
+++ app/src/main/java/com/example/weather_forecast/Activity/WeatherActivity.kt
@@ -1,14 +1,20 @@
 package com.example.weather_forecast.Activity
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weather_forecast.R
 
 class WeatherActivity: AppCompatActivity() {
+    private  val weatherPrisenter = WeatherPrisenter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_weather)
+
+        val temperatureView = findViewById<TextView>(R.id.headTextWeather)
+        temperatureView.text = weatherPrisenter.getWether()
+
     }
 
 }
---
+++ app/src/main/java/com/example/weather_forecast/Activity/WeatherPrisenter.kt
@@ -0,0 +1,7 @@
+package com.example.weather_forecast.Activity
+
+class WeatherPrisenter {
+    fun getWether():String{
+        return "30"
+    }
+}

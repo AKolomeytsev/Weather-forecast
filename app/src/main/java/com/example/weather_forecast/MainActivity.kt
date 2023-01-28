5574b07c5a87b01c98754b5b0124e8b8995e840f
Artem <a.colomeytsev@list.ru>
2023-01-28 13:19:34 +0300

    - Инекции. Не работает

--- app/src/main/java/com/example/weather_forecast/MainActivity.kt
+++ app/src/main/java/com/example/weather_forecast/MainActivity.kt
@@ -2,42 +2,30 @@ package com.example.weather_forecast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.util.Log.DEBUG
 import android.widget.TextView
-import com.example.weather_forecast.BuildConfig.DEBUG
 import com.example.weather_forecast.feature.weather_screen.UI.WeatherScreenPresenter
 import com.example.weather_forecast.feature.weather_screen.WeatherInteractor
-import com.example.weather_forecast.feature.weather_screen.data.cls.WeatherApiClient
 import com.example.weather_forecast.feature.weather_screen.data.cls.WeatherRemoteSource
 import com.example.weather_forecast.feature.weather_screen.data.cls.WeatherRepoImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var presenter : WeatherScreenPresenter
+    private val presenter : WeatherScreenPresenter by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        presenter = WeatherScreenPresenter(WeatherInteractor(WeatherRepoImpl(
-            WeatherRemoteSource(WeatherApiClient.getApi())
-        )))
-
         val outText = findViewById<TextView>(R.id.outText)
         GlobalScope.launch {
-            //Log.d("NET",presenter.interactor.getTemperature())
             withContext(Dispatchers.Main){
                 outText.text = presenter.getWeather()
             }
         }
-
-
-
-
     }
 }
